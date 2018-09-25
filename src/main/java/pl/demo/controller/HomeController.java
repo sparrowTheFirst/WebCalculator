8efc708531c71a85e27f932a6d787c73a8319808
Marcin Wróbel <wrobel.sparrow@gmail.com>
2018-09-25 11:32:21 +0200

    Home controller and application properties file added

---
+++ src/main/java/pl/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Witaj w domu KALKULATOR W PRZEGLĄDARCE!";
+    }
+}
